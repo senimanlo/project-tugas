b29299910d81692738c2b0153fcc74d3b91ceec9
senimanlo <157226347+senimanlo@users.noreply.github.com>
2024-01-20 19:42:50 -0800

    Add files via upload

---
+++ header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <title>jumbotron dengan bootstrap</title>
+   <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<body>
+
+   <nav class="navbar navbar-inverse">
+      <div class="container">
+      <div class="navbar-header">
+         <button type="button" class="navbar-tonggle collapsed" data-tonggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+          <span class="sr-only">Tonggle Nav</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+       </button> 
+       <a class="navbar-brand" href="#"> Penjualan Baju Toko SENI GULO</a>
+    </div>
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+         
+      <li class="active"><a href="home.php"><span class="glyphicon glyphicon-home"></span> HOME <span class="sr-only">(current)</span></a></li>
+        <li><a href="tentang.php"> <span class="glyphicon glyphicon-info-sign"></span> ABOUT</a> </li>
+        <li><a href="produk.php"> <span class="glyphicon glyphicon-shopping-cart"></span> PRODUCT</a></li>
+        <li><a href="haii.php"> <span class="glyphicon glyphicon-tags"></span> NAMA BARANG</a></li>
+        <li><a href="kontak.php"> <span class="glyphicon glyphicon-envelope"></span> CONTACT</a></li>
+         <li><a href="data barang.php"> <span class="glyphicon glyphicon-list-alt"></span> DATA BARANG</a></li>
+          <li><a href="kontak.php"> <span class="glyphicon glyphicon-lock"></span> LOGIN</a></li>
+             </ul>
+             <form class="navbar-form navbar-left">
+             <div class="form-group">
+               <input type="text" class="form-control" placeholder="isikan keyword">
+            </div>
+            <button type="submit" class="btn btn-default">Cari</button>
+         </form>
+         <ul class="nav navbar-nav navbar-right">
+         
+            </ul>
+         </li>
+         </ul>
+      </div>
+   </div>
+</nav>
